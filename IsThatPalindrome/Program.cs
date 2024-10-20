46d35d2414086a39c301933035ea37d8be4634ba
cang8 <0.gizemgunes@gmail.com>
2024-10-20 20:51:50 +0300

    Add project files.

---
+++ IsThatPalindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IsThatPalindrome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir string giriniz: ");
+            string data = Console.ReadLine();
+            bool dataReverse = Palindrome(data);
+            if (dataReverse)
+            {
+                Console.WriteLine("palindrome");
+            }
+            else
+            {
+                Console.WriteLine("palindrome değil");
+            }
+
+        }
+        static bool Palindrome(string input)
+        {
+            int lenght = input.Length;
+
+            for (int i = 0; i < lenght / 2; i++) // t-e-n-e-t --> t == t, e == e,
+            {
+                if (input[i] != input[lenght - i - 1])
+                    return false;
+                else
+                    return true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WordCount/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WordCount
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz:"); //" yarın evi temizledikten sonra markete gideceğim "
+            string sentence = Console.ReadLine();
+            string sentenceTrim = sentence.Trim();
+            string[] data = sentenceTrim.Split(' ');
+            
+            int length = data.Length;
+            Console.WriteLine($"Yazdiginiz cümledeki kelime sayisi: {length}");
+        }
+    }
+}
